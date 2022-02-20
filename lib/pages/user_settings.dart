06c9671fab072f738645fa356ea36d87f30f4f55
GUMBAUElric <elric.gumbau@ynov.com>
2022-02-20 15:27:41 +0100

    feat(userSettings): create component & layout user settings

--- lib/pages/user_settings.dart
+++ lib/pages/user_settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/base/base.dart';
 import 'package:time_tracker/layouts/app_bar.dart';
+import 'package:time_tracker/layouts/logout_form.dart';
 
 class UserSettings extends StatelessWidget {
   const UserSettings({Key? key}) : super(key: key);
@@ -8,13 +9,20 @@ class UserSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const TTAppBar(
-        title: Text("Settings", style: TextStyle(color: TTColors.primary)),
+      backgroundColor: Colors.white,
+      appBar: TTAppBar(
+        title: SizedBox(
+          width: 140,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Text("Settings", style: TextStyle(color: TTColors.primary)),
+              Icon(Icons.settings, color: TTColors.primary)
+            ],
+          ),
+        ),
       ),
-      body: Container(
-        color: Colors.white,
-        child: const Text("User Settings page :)"),
-      ),
+      body: const TTLogoutForm(),
     );
   }
 }
