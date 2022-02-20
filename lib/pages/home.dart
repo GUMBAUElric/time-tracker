c5240c9b0c4d7b4a53f8f75c554d3dda98be8308
GUMBAUElric <elric.gumbau@ynov.com>
2022-02-20 19:54:34 +0100

    refactor: center Scaffold body

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/base/base.dart';
 import 'package:time_tracker/components/layout_slider.dart';
+import 'package:time_tracker/layouts/add_tasks.dart';
 import 'package:time_tracker/layouts/app_bar.dart';
 import 'package:time_tracker/layouts/nav_bar.dart';
 
 class Home extends StatelessWidget {
   final List<Map<String, dynamic>> _navigationLayouts = [
     {"icon": Icons.home_rounded, "layout": const Text("Home")},
-    {"icon": Icons.add_rounded, "layout": const Text("Add Tasks")}
+    {"icon": Icons.add_rounded, "layout": const AddTasks()}
   ];
 
   Home({Key? key}) : super(key: key);
@@ -18,15 +19,13 @@ class Home extends StatelessWidget {
         LayoutSlider(navigationLayouts: _navigationLayouts);
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: const TTAppBar(
         title: Text('Time Tracker', style: TextStyle(color: TTColors.primary)),
         leading: Icon(Icons.notifications_rounded,
             color: TTColors.primary, size: 25),
       ),
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        child: _layoutSlider,
-      ),
+      body: _layoutSlider,
       bottomNavigationBar: TTNavBar(
           navigationLayouts: _navigationLayouts,
           callback: (currentIndex) => _layoutSlider.goToPage(currentIndex)),
