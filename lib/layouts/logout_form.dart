28d80a319e4de8dd9c44df88127af52bbfb437a9
GUMBAUElric <elric.gumbau@ynov.com>
2022-02-20 16:12:52 +0100

    feat(user): add connectedAt attribute

--- lib/layouts/logout_form.dart
+++ lib/layouts/logout_form.dart
@@ -5,6 +5,7 @@ import 'package:redux/redux.dart';
 import 'package:time_tracker/base/base.dart';
 import 'package:time_tracker/components/button.dart';
 import 'package:time_tracker/components/dialog.dart';
+import 'package:time_tracker/models/user.model.dart';
 import 'package:time_tracker/store/tt.actions.dart';
 import 'package:time_tracker/store/tt.state.dart';
 
@@ -15,46 +16,53 @@ class TTLogoutForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreBuilder<TimeTrackerState>(
       builder: (BuildContext context, Store<TimeTrackerState> store) {
+        UserModel? user = store.state.user;
+
         return SizedBox(
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text(
+                user!.pseudo,
+                style: const TextStyle(fontSize: 18),
+              ),
               Container(
-                  margin: const EdgeInsets.only(bottom: 30),
-                  child: Text(
-                    store.state.user!.pseudo,
-                    style: const TextStyle(fontSize: 18),
-                  )),
-              TTButton(
-                  padding: 15,
-                  width: 120,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Text("Logout"),
-                      Icon(
-                        Icons.logout_rounded,
-                        size: 20,
-                      )
-                    ],
-                  ),
-                  backgroundColor: TTColors.tertiary,
-                  onPressed: () {
-                    TTDialog(context: context, actions: [
-                      CupertinoDialogAction(
-                          child: const Text("No"),
-                          onPressed: () => Navigator.pop(context)),
-                      CupertinoDialogAction(
-                          child: const Text("Yes"),
-                          onPressed: () {
-                            store.dispatch(LogOutUser());
+                margin: const EdgeInsets.symmetric(vertical: 30),
+                child: TTButton(
+                    padding: 15,
+                    width: 120,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Text("Logout"),
+                        Icon(
+                          Icons.logout_rounded,
+                          size: 20,
+                        )
+                      ],
+                    ),
+                    backgroundColor: TTColors.tertiary,
+                    onPressed: () {
+                      TTDialog(context: context, actions: [
+                        CupertinoDialogAction(
+                            child: const Text("No"),
+                            onPressed: () => Navigator.pop(context)),
+                        CupertinoDialogAction(
+                            child: const Text("Yes"),
+                            onPressed: () {
+                              store.dispatch(LogOutUser());
 
-                            Navigator.pushNamedAndRemoveUntil(
-                                context, "/", (route) => false);
-                          }),
-                    ]).show();
-                  })
+                              Navigator.pushNamedAndRemoveUntil(
+                                  context, "/", (route) => false);
+                            }),
+                      ]).show();
+                    }),
+              ),
+              Text(
+                "Last connection : ${user.connectedAt}",
+                style: const TextStyle(fontSize: 15),
+              ),
             ],
           ),
         );
--- lib/models/user.model.dart
+++ lib/models/user.model.dart
@@ -1,5 +1,8 @@
+import 'package:intl/intl.dart';
+
 class UserModel {
   String pseudo;
+  String connectedAt = DateFormat('dd/MM/yyyy HH:mm').format(DateTime.now());
 
-  UserModel({ required this.pseudo });
+  UserModel({required this.pseudo});
 }
