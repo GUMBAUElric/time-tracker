65eda5bd094176a7482feceab3ebb976a344eb8b
GUMBAUElric <elric.gumbau@ynov.com>
2022-03-03 20:36:04 +0100

    feat: create header dashboard component

--- lib/layouts/dashboard.dart
+++ lib/layouts/dashboard.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-import 'package:time_tracker/components/button.dart';
 import 'package:time_tracker/components/container.dart';
+import 'package:time_tracker/components/dashboard/header.dart';
 import 'package:time_tracker/models/task.model.dart';
 import 'package:time_tracker/store/tt.state.dart';
 import 'package:time_tracker/utils/date.dart';
 
 class TTDashboard extends StatelessWidget {
-  final String currentDate = getDashBoardDate();
-
-  TTDashboard({Key? key}) : super(key: key);
+  const TTDashboard({Key? key}) : super(key: key);
 
   List<TaskModel> getTasksByCurrentDay(List<TaskModel> tasks) {
     String _currentDay = getCurrentDate().split("/")[0];
@@ -29,30 +27,11 @@ class TTDashboard extends StatelessWidget {
 
         return TTContainer(
           height: double.infinity,
+          width: 700,
           marginHorizontal: 19,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(currentDate,
-                      style: const TextStyle(
-                          fontSize: 30, fontWeight: FontWeight.bold)),
-                  TTButton(
-                      child: Text(
-                          _tasks.length > 1
-                              ? "${_tasks.length} tasks"
-                              : "${_tasks.length} task",
-                          style: const TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500)),
-                      padding: 2,
-                      width: 90,
-                      height: 38)
-                ],
-              )
-            ],
+            children: [TTDashBoardHeader(numberOfTasks: _tasks.length)],
           ),
         );
       },
