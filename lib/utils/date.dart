86b7e64cd2738ca2e9d4dc6fe4fa39568c5c07a5
GUMBAUElric <elric.gumbau@ynov.com>
2022-03-04 00:15:39 +0100

    feat: create getMonthDays & getCurrentDay functions

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -8,6 +8,10 @@ String getCurrentDate() {
   return DateFormat('dd/MM/yyyy').format(DateTime.now()).toString();
 }
 
+int getCurrentDay() {
+  return DateTime.now().day;
+}
+
 String getCurrentTime() {
   return DateFormat('HH:mm').format(DateTime.now()).toString();
 }
@@ -36,4 +40,35 @@ bool timeRangeIsValid(String startTime, String endTime) {
   if (endHour == startHour && endMinutes > startMinutes) return true;
 
   return false;
+}
+
+List<Map> getMonthDays() {
+  List<int> _daysIndex = [];
+  List<Map> monthDays = [];
+
+  int _currentYear = DateTime.now().year;
+  int _currentMonth = DateTime.now().month;
+  int _currentDay = DateTime.now().day;
+
+  int _nbOfDays = DateTime(_currentYear, _currentMonth + 1, 0).day;
+  int _remainingDays = _nbOfDays - _currentDay;
+
+  for (int i = -1; i > -_currentDay; i--) {
+    _daysIndex.add(i);
+  }
+
+  _daysIndex = _daysIndex.reversed.toList();
+
+  for (int i = 0; i <= _remainingDays; i++) {
+    _daysIndex.add(i);
+  }
+
+  for (int i = 0; i < _daysIndex.length; i++) {
+    String _dayName = DateFormat('EEE').format(
+        DateTime(_currentYear, _currentMonth, _currentDay + _daysIndex[i]));
+
+    monthDays.add({"dayNumber": "${i + 1}", "dayName": _dayName});
+  }
+
+  return monthDays;
 }
