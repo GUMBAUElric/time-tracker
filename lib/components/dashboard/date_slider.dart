e3530fc9f0936d9e7bd4560d3c90eeb74cdcf2ad
GUMBAUElric <elric.gumbau@ynov.com>
2022-03-04 00:15:16 +0100

    feat: create TTDashBoardDateSlider component

---
+++ lib/components/dashboard/date_slider.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:time_tracker/base/base.dart';
+import 'package:time_tracker/components/container.dart';
+import 'package:time_tracker/utils/date.dart';
+
+class TTDashBoardDateSlider extends StatefulWidget {
+  const TTDashBoardDateSlider({Key? key}) : super(key: key);
+
+  @override
+  State<TTDashBoardDateSlider> createState() => _TTDashBoardDateSliderState();
+}
+
+class _TTDashBoardDateSliderState extends State<TTDashBoardDateSlider> {
+  final itemController = ItemScrollController();
+  late int currentIndex = 0;
+  final List<Map> _days = getMonthDays();
+
+  @override
+  void initState() {
+    setDefaultSelectedDate();
+  }
+
+  void setDefaultSelectedDate() {
+    currentIndex = getCurrentDay() - 1;
+
+    WidgetsBinding.instance!.addPostFrameCallback(
+        (_) => itemController.jumpTo(index: currentIndex));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TTContainer(
+      width: double.infinity,
+      marginHorizontal: 0,
+      height: 70,
+      child: ScrollablePositionedList.builder(
+        itemScrollController: itemController,
+        itemCount: _days.length,
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => setState(() => currentIndex = index),
+            child: Container(
+                decoration: (index == currentIndex)
+                    ? BoxDecoration(
+                        border: Border.all(color: TTColors.primary),
+                        borderRadius: BorderRadius.circular(10))
+                    : null,
+                width: 50,
+                margin: const EdgeInsets.only(right: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(_days[index]["dayNumber"],
+                        style: TextStyle(
+                            color: (index == currentIndex)
+                                ? TTColors.primary
+                                : TTColors.primary.withOpacity(0.5),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold)),
+                    Text(_days[index]["dayName"],
+                        style: TextStyle(
+                            color: (index == currentIndex)
+                                ? TTColors.primary
+                                : TTColors.primary.withOpacity(0.5),
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                )),
+          );
+        },
+      ),
+    );
+  }
+}
